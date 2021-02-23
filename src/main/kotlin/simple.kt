589a25ee6e089061cd2dc38be2ea51aeb15ce29a
Tuan Chau <chau.vinh.tuan@linecorp.com>
2021-02-24 07:43:15 +0900

    DSL with click event

--- src/main/kotlin/simple.kt
+++ src/main/kotlin/simple.kt
@@ -1,13 +1,19 @@
 import kotlinx.browser.document
 import kotlinx.browser.window
+import kotlinx.html.InputType
+import kotlinx.html.button
 import kotlinx.html.dom.append
+import kotlinx.html.input
 import kotlinx.html.js.a
 import kotlinx.html.js.b
 import kotlinx.html.js.h1
 import kotlinx.html.js.i
+import kotlinx.html.js.onClickFunction
 import kotlinx.html.js.p
 import kotlinx.html.js.span
 import kotlinx.html.style
+import org.w3c.dom.HTMLButtonElement
+import org.w3c.dom.HTMLInputElement
 
 fun main() {
     window.onload = {
@@ -48,8 +54,23 @@ fun onLoad() {
                 +"a link"
             }
         }
-    }
 
-    console.log(root)
-    println(root.innerHTML)
+        p {
+            button {
+                onClickFunction = {
+                    window.alert("Clicked!")
+                    (it.target as HTMLButtonElement).textContent = "You clicked me!"
+                }
+                +"Click me"
+            }
+        }
+        p {
+            input(InputType.button) {
+                value = "Click me 2"
+                onClickFunction = {
+                    (it.target as HTMLInputElement).value = "You clicked me!"
+                }
+            }
+        }
+    }
 }
